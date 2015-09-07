9806466e57d7fb2c579aceb6696f89eb82bbbedc
Tan Theng Lee <thengleetan@gmail.com>
2015-09-08 01:03:20 +0800

    Add stock to products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150907075515) do
+ActiveRecord::Schema.define(version: 20150907144131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,8 +21,9 @@ ActiveRecord::Schema.define(version: 20150907075515) do
     t.decimal  "price",       precision: 8, scale: 2
     t.text     "description"
     t.string   "image_url"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+    t.integer  "stock",                               default: 0
   end
 
 end
