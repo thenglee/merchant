af69955b6f9051424f196371b1957bd66c563dd1
Tan Theng Lee <thengleetan@gmail.com>
2015-09-10 21:56:00 +0800

    Associate orders with users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,13 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def load_order
+    @order = Order.find_or_initialize_by(id: session[:order_id], status: "unsubmitted")
+
+    if @order.new_record?
+      @order.save!
+      session[:order_id] = @order.id
+    end
+  end
 end
