b3058fc5bcad563d4e3f705837ad60a27b178b97
Tan Theng Lee <thengleetan@gmail.com>
2015-10-18 16:41:40 +0800

    Refactor product instance creation into before block

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -7,59 +7,58 @@ RSpec.describe Product, type: :model do
   end
 
   describe "validations" do
+    before do
+      @product = Product.new(title: "Product", price: 10, description: "product text", image_url: "img1.png", stock: 10)
+    end
+
     it "should have a title" do
-      product = Product.new(title: "Product", price: 10, description: "product text", image_url: "img1.png", stock: 10)
-      product.title = nil
-      expect(product.valid?).to eq false
+      @product.title = nil
+      expect(@product.valid?).to eq false
     end
 
     it "should have a price" do
-      product = Product.new(title: "Product", price: 10, description: "product text", image_url: "img1.png", stock: 10)
-      product.price = nil
-      expect(product.valid?).to eq false
+      @product.price = nil
+      expect(@product.valid?).to eq false
     end
 
     it "should have a description" do
-      product = Product.new(title: "Product", price: 10, description: "product text", image_url: "img1.png", stock: 10)
-      product.description = nil
-      expect(product.valid?).to eq false
+      @product.description = nil
+      expect(@product.valid?).to eq false
     end
 
     it "should have a image_url" do
-      product = Product.new(title: "Product", price: 10, description: "product text", image_url: "img1.png", stock: 10)
-      product.image_url = nil
-      expect(product.valid?).to eq false
+      @product.image_url = nil
+      expect(@product.valid?).to eq false
     end
 
     it "should have a stock" do
-      product = Product.new(title: "Product", price: 10, description: "product text", image_url: "img1.png", stock: 10)
-      product.stock = nil
-      expect(product.valid?).to eq false
+      @product.stock = nil
+      expect(@product.valid?).to eq false
     end
 
     it "should have a numerical value for price" do
-      product = Product.new(title: "Product", price: "ten", description: "product text", image_url: "img1.png", stock: 10)
-      expect(product.valid?).to eq false
+      @product.price = "ten"
+      expect(@product.valid?).to eq false
     end
 
     it "should have a positive value for price" do
-      product = Product.new(title: "Product", price: -10, description: "product text", image_url: "img1.png", stock: 10)
-      expect(product.valid?).to eq false
+      @product.price = -10
+      expect(@product.valid?).to eq false
     end
 
     it "should have a numerical value for stock" do
-      product = Product.new(title: "Product", price: 10, description: "product text", image_url: "img1.png", stock: "ten")
-      expect(product.valid?).to eq false
+      @product.stock = "ten"
+      expect(@product.valid?).to eq false
     end
 
     it "should have an integer value for stock" do
-      product = Product.new(title: "Product", price: 10, description: "product text", image_url: "img1.png", stock: 10.50)
-      expect(product.valid?).to eq false
+      @product.stock = 10.50
+      expect(@product.valid?).to eq false
     end
 
     it "should have a positive integer value for stock" do
-      product = Product.new(title: "Product", price: 10, description: "product text", image_url: "img1.png", stock: -10)
-      expect(product.valid?).to eq false
+      @product.stock = -10
+      expect(@product.valid?).to eq false
     end
   end
 end
