7efa7a2b4b61499829d2c888f68a0713b9eccdd4
Tan Theng Lee <thengleetan@gmail.com>
2015-10-19 20:32:23 +0800

    Refactor order_item creation into before block in validation tests

--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -12,34 +12,38 @@ RSpec.describe OrderItem, type: :model do
   end
 
   describe "validations" do
+    before do
+      @order_item = OrderItem.new(order_id: @order.id, product_id: @product.id, quantity: 10)
+    end
+
     it "should have an order" do
-      order_item = OrderItem.new(product_id: @product.id, quantity: 10)
-      expect(order_item.valid?).to eq false
+      @order_item.order_id = nil
+      expect(@order_item.valid?).to eq false
     end
 
     it "should have a product" do
-      order_item = OrderItem.new(order_id: @order.id, quantity: 10)
-      expect(order_item.valid?).to eq false
+      @order_item.product_id = nil
+      expect(@order_item.valid?).to eq false
     end
 
     it "should have quantity" do
-      order_item = OrderItem.create(product_id: @product.id, order_id: @order.id)
-      expect(order_item.valid?).to eq false
+      @order_item.quantity = nil
+      expect(@order_item.valid?).to eq false
     end
 
     it "should have a numerical value for quantity" do
-      order_item = OrderItem.create(product_id: @product.id, order_id: @order.id, quantity: "ten")
-      expect(order_item.valid?).to eq false
+      @order_item.quantity = "ten"
+      expect(@order_item.valid?).to eq false
     end
 
     it "should have an integer value for quantity" do
-      order_item = OrderItem.create(product_id: @product.id, order_id: @order.id, quantity: 10.50)
-      expect(order_item.valid?).to eq false
+      @order_item.quantity = 10.50
+      expect(@order_item.valid?).to eq false
     end
 
     it "should have a positive integer value for quantity" do
-      order_item = OrderItem.create(product_id: @product.id, order_id: @order.id, quantity: -10)
-      expect(order_item.valid?).to eq false
+      @order_item.quantity = -10
+      expect(@order_item.valid?).to eq false
     end
   end
 
