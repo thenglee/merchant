9630570d431ebc1731f13320e0c511e663943ea5
Tan Theng Lee <thengleetan@gmail.com>
2015-10-19 23:50:53 +0800

    Add validation of address for happy case

---
+++ spec/models/address_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+RSpec.describe Address, type: :model do
+  before do
+    @user = User.new(provider: "twitter", uid: "0123456789", name: "name")
+  end
+
+  it "is valid" do
+    address = Address.new(line1: "line1", line2: "line2", city: "city", state: "WA", zip: "12345", user_id: @user.id)
+    expect(address.valid?).to eq true
+  end
+end
