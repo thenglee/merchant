afdaa4ce0d30ae94a46cc80ab9d082272fc27d07
Tan Theng Lee <thengleetan@gmail.com>
2015-10-20 18:38:11 +0800

    Add test for User association with Order

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,19 @@ RSpec.describe User, type: :model do
     user = User.new(provider: "twitter", uid: "0123456789", name: "name")
     expect(user.valid?).to eq true
   end
+
+  describe "model associations" do
+    before do
+      @user = User.create(provider: "twitter", uid: "0123456789", name: "name")
+    end
+
+    it "should have orders" do
+      expect(@user.respond_to?(:orders)).to eq true
+    end
+
+    it "should return its orders" do
+      order = Order.create(status: "unsubmitted", user_id: @user.id)
+      expect(@user.orders).to eq [order]
+    end
+  end
 end
