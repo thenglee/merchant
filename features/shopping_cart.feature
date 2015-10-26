a147354494ad88e328266d19f4e77f7c1c467116
Tan Theng Lee <thengleetan@gmail.com>
2015-10-26 20:27:23 +0800

    Add steps details for passing cukes

--- features/shopping_cart.feature
+++ features/shopping_cart.feature
@@ -5,8 +5,10 @@ Feature: Visit store and to select and buy items
   Users should be able to check out their cart and make payment.
 
   Scenario: View an item's details
-    Given the user is at the products catalogue page
-    When the user clicks on the Oranges link
-    Then the user should see the Oranges item details
-    And the user should see the back URL
+    Given there is an 'Oranges' item in the products list
+    When the user goes to the products URL
+    Then the user should see the 'Oranges' item in the product catalogue
+    When the user clicks on the 'Oranges' link
+    Then the user should see the 'Oranges' item details
+    And the user should see the 'Back' link
     And the user should be able to go back to the products catalogue page
--- features/step_definitions/shopping_cart_steps.rb
+++ features/step_definitions/shopping_cart_steps.rb
@@ -1,19 +1,31 @@
-Given(/^the user is at the products catalogue page$/) do
-  pending # express the regexp above with the code you wish you had
+Given(/^there is a 'Oranges' item in the products list$/) do
+  Product.create(title: "Oranges", price: 2.99, description: "Bag of 6 Valencia oranges", image_url: "oranges.jpg", stock: 50)
 end
 
-When(/^the user clicks on the Oranges link$/) do
-  pending # express the regexp above with the code you wish you had
+When(/^the user goes to the products URL$/) do
+  visit products_path
 end
 
-Then(/^the user should see the Oranges item details$/) do
-  pending # express the regexp above with the code you wish you had
+Then(/^the user should see the 'Oranges' item in the product catalogue$/) do
+  page.should have_content('Oranges')
 end
 
-Then(/^the user should see the back URL$/) do
-  pending # express the regexp above with the code you wish you had
+When(/^the user clicks on the 'Oranges' link$/) do
+  click_link 'Oranges'
+end
+
+Then(/^the user should see the 'Oranges' item details$/) do
+  page.should have_content('Title: Oranges')
+  page.should have_content('Price: $2.99')
+  page.should have_content('Description: Bag of 6 Valencia oranges')
+  page.should have_content('Stock: 50 In Stock')
+end
+
+Then(/^the user should see the 'Back' link$/) do
+  find_link('Back').visible?
 end
 
 Then(/^the user should be able to go back to the products catalogue page$/) do
-  pending # express the regexp above with the code you wish you had
+  click_link 'Back'
+  expect(current_path).to eq products_path
 end
