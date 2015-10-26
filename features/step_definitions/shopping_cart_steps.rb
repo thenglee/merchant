c1b0d84cef587e05fb5233023202760d9dc8b5d1
Tan Theng Lee <thengleetan@gmail.com>
2015-10-26 17:31:41 +0800

    Add generated cuke steps

---
+++ features/step_definitions/shopping_cart_steps.rb
@@ -0,0 +1,19 @@
+Given(/^the user is at the products catalogue page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^the user clicks on the Oranges link$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the user should see the Oranges item details$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the user should see the back URL$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the user should be able to go back to the products catalogue page$/) do
+  pending # express the regexp above with the code you wish you had
+end
