2cde10abc9db079d2c223c81c2b60db8e40ac74f
Tan Theng Lee <thengleetan@gmail.com>
2015-11-07 16:36:09 +0800

    Add seed data for products

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# db/seeds.rb
+
+Product.create(title: "Oranges", price: 2.99, description: "Bag of 6 Valencia oranges", image_url: "oranges.jpg", stock: 100)
+Product.create(title: "Arugula", price: 2.49, description: "Arugula", image_url: "arugula.jpg", stock: 100)
+Product.create(title: "Broccoli", price: 5.59, description: "Broccoli", image_url: "broccoli.jpg", stock: 100)
+Product.create(title: "Carrots", price: 2.50, description: "Carrots", image_url: "carrots.jpg", stock: 100)
+Product.create(title: "Cucumber", price: 2.00, description: "Cucumber", image_url: "cucumber.jpg", stock: 100)
+Product.create(title: "Eggs", price: 4.00, description: "Pack of 6 eggs", image_url: "eggs.jpg", stock: 100)
+Product.create(title: "Green Apples", price: 3.00, description: "Pack of 4 green apples", image_url: "green_apples.jpg", stock: 100)
+Product.create(title: "Green Grapes", price: 4.50, description: "1 bunch of green grapes", image_url: "green_grapes.jpg", stock: 100)
+Product.create(title: "Milk", price: 2.49, description: "1 carton of milk", image_url: "milk.jpg", stock: 100)
+Product.create(title: "Purple Grapes", price: 4.99, description: "1 bunch of purple grapes", image_url: "purple_grapes.jpg", stock: 100)
+Product.create(title: "Romaine", price: 3.50, description: "Romaine", image_url: "romaine.jpg", stock: 100)
+Product.create(title: "Swiss Cheese", price: 9.99, description: "100g of Swiss Cheese", image_url: "swiss_cheese.jpg", stock: 100)
